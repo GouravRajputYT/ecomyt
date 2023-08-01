68548004caaeeb3b5a0a99839f8be34bc03ad3dc
Gourav <gouravrajputpersonal@gmail.com>
2023-08-01 12:08:50 +0530

    Initial Commit

---
+++ accounts/models.py
@@ -0,0 +1,69 @@
+from django.db import models
+from django.contrib.auth.models import User
+from base.models import BaseModel
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+import uuid
+from base.emails import send_account_activation_email
+from products.models import Product, ColorVarient, SizeVarient
+
+
+class Profile(BaseModel):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name="profile")
+    is_email_verified = models.BooleanField(default=False)
+    email_token = models.CharField(max_length=100, null=True, blank=True)
+    profile_image = models.ImageField(upload_to = "profile")
+
+    def get_cart_count(self):
+        return CartItems.objects.filter(cart__is_paid = False, cart__user = self.user).count()
+
+class Cart(BaseModel):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="carts" )
+    is_paid = models.BooleanField(default=False)
+
+    def get_cart_total(self):
+        cart_items = self.cart_items.all()
+        price = []
+        for cart_item in cart_items:
+            price.append(cart_item.product.price)
+            if cart_item.color_varient:
+                color_varient_price = cart_item.color_varient.price
+                price.append(color_varient_price)
+            if cart_item.size_varient:
+                size_varient_price = cart_item.size_varient.price
+                price.append(size_varient_price)
+                
+        return sum(price)
+
+
+
+class CartItems(BaseModel):
+    cart = models.ForeignKey(Cart, on_delete=models.CASCADE, related_name="cart_items")
+    product = models.ForeignKey(Product, on_delete=models.SET_NULL, null=True, blank = True)
+    color_varient = models.ForeignKey(ColorVarient, on_delete=models.SET_NULL, null=True, blank = True)
+    size_varient = models.ForeignKey(SizeVarient, on_delete=models.SET_NULL, null=True, blank = True)
+
+    def get_product_price(self):
+        price = [seld.product.price]
+
+        if self.color_varient:
+            color_varient_price = self.color_varient.price
+            price.append(color_varient_price)
+        if self.size_varient:
+            size_varient_price = self.size_varient.price
+            price.append(size_varient_price)
+        return sum(price)
+
+
+@receiver(post_save, sender = User)
+
+def send_email_token(sender, instance, created, **kwargs):
+    try:
+        if created:
+            email_token = str(uuid.uuid4())
+            Profile.objects.create(user=instance, email_token=email_token)
+            email = instance.email
+            send_account_activation_email(email, email_token)
+
+    except Exception as e:
+        print(e)
